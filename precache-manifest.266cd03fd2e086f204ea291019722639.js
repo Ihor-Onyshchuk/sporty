9877dd82fe042943b52f261889cd89892aca211c
ihor <onyshchukihor11@gmail.com>
2020-06-21 16:23:41 +0300

    Updates

---
+++ precache-manifest.266cd03fd2e086f204ea291019722639.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081355924e49f2aa2aa95acd60daca40",
+    "url": "/sporty/index.html"
+  },
+  {
+    "revision": "a63a976799015e24e68c",
+    "url": "/sporty/static/css/main.e99d8422.chunk.css"
+  },
+  {
+    "revision": "1d1328688e4fac2f050b",
+    "url": "/sporty/static/js/2.e43ae797.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sporty/static/js/2.e43ae797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63a976799015e24e68c",
+    "url": "/sporty/static/js/main.a09a0e32.chunk.js"
+  },
+  {
+    "revision": "6811450d4bbb08dcfd2d",
+    "url": "/sporty/static/js/runtime-main.3b43c592.js"
+  },
+  {
+    "revision": "bd85ac8c8c40fdad68cfe0101a744c8a",
+    "url": "/sporty/static/media/load-indicator.bd85ac8c.gif"
+  }
+]);
