a87cc21bcc8e45b2d7211678d5abad25ee6f8e19
ihor <onyshchukihor11@gmail.com>
2020-06-23 13:16:29 +0300

    add AnimatedCard component

---
+++ src/components/AnimatedCard/AnimatedCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import T from 'prop-types'
+import { Spring } from 'react-spring/renderprops'
+
+
+const AnimatedCard = ({ card: Card, cardProps, index = 0 }) => {
+  return (
+    <Spring
+      from={{ scale: '0' }}
+      to={{ scale: '1' }}
+      config={{ delay: index * 100, duration: 300 }}
+    >
+      {springProps => (
+        <Card {...cardProps} style={springProps} />
+      )}
+    </Spring>
+  )
+}
+
+AnimatedCard.propTypes = {
+  card: T.elementType.isRequired,
+  cardProps: T.object.isRequired,
+  index: T.number
+}
+
+export default AnimatedCard
--- src/components/ClubList/ClubList.js
+++ src/components/ClubList/ClubList.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import T from 'prop-types';
-import { Spring } from 'react-spring/renderprops';
 
 import ClubCard from '../ClubCard/ClubCard';
+import AnimatedCard from '../AnimatedCard/AnimatedCard'
 
 const ClubList = ({ clubs = [] }) => (
   <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3 my-3">
     {clubs.map((club, index) => (
       <div className="col mb-4" key={club.title}>
-        <Spring
-          from={{ scale: '0' }}
-          to={{ scale: '1' }}
-          config={{ delay: index * 100, duration: 500 }}
-        >
-          {springProps => (
-            <ClubCard club={club} style={springProps} />
-          )}
-        </Spring>
+        <AnimatedCard card={ClubCard} cardProps={{ club }} index={index} />
       </div>
     ))}
   </div>
